9d204bef4428ea7a0a9f09b896000bc9fa06a708
Mariusz Ozdoba <mariuszozdoba@e902ab1f.local>
2019-05-02 17:50:00 +0200

    launch script

--- start.py
+++ start.py
@@ -1,25 +1,33 @@
 import os
 from pathlib import Path
+import shutil
+
+#Make folder for all
+os.mkdir('../' 'BenchmarkPyWebFrameworks')
+
+os.chdir('../BenchmarkPyWebFrameworks')
+
+shutil.move('ComparePyWebFrameworks', '.')
 
 os.system('git clone https://github.com/comparepywebframework/Django_framework.git ..')
 os.system('git clone https://github.com/comparepywebframework/Flask_framework.git ..')
 os.system('git clone https://github.com/comparepywebframework/Pyramid_framework.git ..')
 
-django_framework = Path('../Django_framework')
-flask_framework = Path('../Flask_framework')
-pyramid_framework = Path('../Pyramid_framework')
+django_framework = Path('BenchmarkPyWebFrameworks/Django_framework')
+flask_framework = Path('BenchmarkPyWebFrameworks/Flask_framework')
+pyramid_framework = Path('BenchmarkPyWebFrameworks/Pyramid_framework')
 
 #Build main service
-os.system('docker-compose up -d')
+os.system('docker-compose up -d --build')
 
 #Build Django_framework service
 os.chdir(str(django_framework))
-os.system('docker-compose up -d')
+os.system('docker-compose up -d --build')
 
 #Build Flask_framework service
 os.chdir(str(flask_framework))
-os.system('docker-compose up -d')
+os.system('docker-compose up -d --build')
 
 #Build Pyramid_framework service
 os.chdir(str(pyramid_framework))
-os.system('docker-compose up -d')
+os.system('docker-compose up -d --build')
